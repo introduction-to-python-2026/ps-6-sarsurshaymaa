6a30810c52aeed376a59a08d3b127977c224c248
sarsurshaymaa <sarsurshaymaa@gmail.com>
2025-11-27 14:19:59 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,11 +1,11 @@
 def create_codon_dict(file_path):
  codonaa = {}
- with open(file_path, "r") as file:
-        rows = file.readlines()
-    for r in rows[:] : 
-        cell = r.strip().split('\t')
-        key = cell[0]      
-        value = cell[2]    
-        codonaa[key] = value
-  return codonaa
+with open(file_path, "r") as file:
+     rows = file.readlines()
+for r in rows[:] : 
+     cell = r.strip().split('\t')
+     key = cell[0]      
+     value = cell[2]    
+     codonaa[key] = value
+return codonaa
 
